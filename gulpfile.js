c6fe80c7d0396e274f188267c93803babc30c2f6
mikhail-kalinin <allgoodyeah@gmail.com>
2017-03-29 23:02:30 +0800

    Написал автоматическую сборку личного проекта, не работает. Ругается на что то с mqpacker

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,35 @@
 "use strict";
 
 var gulp = require("gulp");
+var precss = require("precss");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-var precss = require("precss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var jsmin = require('gulp-jsmin');
+var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
+var svgmin = require("gulp-svgmin");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
@@ -15,20 +39,64 @@ gulp.task("style", function() {
       autoprefixer({browsers: [
         "last 2 versions"
       ]})
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("script", function () {
+  gulp.src("build/js/script.js")
+    .pipe(jsmin())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+}
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("svgmin", function() {
+  return gulp.src("build/img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", function (fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "script",
+    "images",
+    "svgmin",
+    fn)
+});
+
+gulp.task("html:copy", function() {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+
+.gulp.task("html:update" ["html:copy"], function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
   });
 
   gulp.watch("postcss/**/*.css", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", [html:update]);
 });
